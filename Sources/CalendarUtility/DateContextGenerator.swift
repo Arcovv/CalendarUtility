8fb83ca4d18c15feedfd3e011fd139b1275482a3
Arco <yiyezhihen@gmail.com>
2019-10-20 13:20:12 +0800

    Add DateContextGeneratorTests and update DateContextGenerator

--- Sources/CalendarUtility/DateContextGenerator.swift
+++ Sources/CalendarUtility/DateContextGenerator.swift
@@ -1,8 +1,23 @@
 import Foundation
 
 public struct DateContextGenerator {
+  
+  /// Generate date contexts between two days.
+  /// - **NOTE**: startDate should smaller or equal with endDate, otherwise return empty array.
+  /// - Parameter calendar: The Calendar object
+  /// - Parameter startDate: The started date
+  /// - Parameter endDate: The ended date
   public static func make(calendar: Calendar = .current, startDate: Date, endDate: Date) -> [DateContext] {
+    guard startDate <= endDate else {
+      return []
+    }
+    
     var result = [DateContext(date: startDate)]
+    
+    if startDate == endDate {
+      return result
+    }
+    
     var nextDate = calendar.date(byAdding: .day, value: 1, to: startDate)!
     
     repeat {
---
+++ Tests/CalendarUtilityTests/DateContextGeneratorTests.swift
@@ -0,0 +1,31 @@
+import XCTest
+@testable import CalendarUtility
+
+final class DateContextGeneratorTests: XCTestCase {
+  
+  let calendar = Calendar(identifier: .gregorian)
+  
+  func testFrom20191006To20191019CountIs14() {
+    let startDate = createDate(year: 2019, month: 10, day: 06)
+    let endDate = createDate(year: 2019, month: 10, day: 19)
+    let sut = DateContextGenerator.make(calendar: calendar, startDate: startDate, endDate: endDate)
+    
+    XCTAssertEqual(sut.count, 14)
+  }
+  
+  func testStartDateIsTheSameWithEndDateCountIs1() {
+    let startDate = createDate(year: 2019, month: 10, day: 06)
+    let endDate = startDate
+    let sut = DateContextGenerator.make(calendar: calendar, startDate: startDate, endDate: endDate)
+    
+    XCTAssertEqual(sut.count, 1)
+  }
+  
+  func testEndDateIsSmallerThanStartDateCount0() {
+    let endDate = createDate(year: 2019, month: 10, day: 06)
+    let startDate = createDate(year: 2019, month: 10, day: 19)
+    let sut = DateContextGenerator.make(calendar: calendar, startDate: startDate, endDate: endDate)
+    
+    XCTAssertEqual(sut.count, 0)
+  }
+}
